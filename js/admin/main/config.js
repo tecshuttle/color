bfb4952afe06759643ee14b0813c738185edd8ce
tecshuttle <tecshuttle@gmail.com>
2015-12-08 18:40:11 +0800

    整理好管理后台菜单。

--- js/admin/main/config.js
+++ js/admin/main/config.js
@@ -21,8 +21,18 @@ Docs.Menu = [
                 leaf: true
             },
             {
-                href: "/admin/products",
-                text: "产品管理",
+                href: "/admin/devices",
+                text: "越野器材",
+                leaf: true
+            },
+            {
+                href: "/admin/branch",
+                text: "企业版图",
+                leaf: true
+            },
+            {
+                href: "/admin/jobs",
+                text: "人才招聘",
                 leaf: true
             }
         ]
@@ -40,52 +50,10 @@ Docs.Menu = [
         ]
     },
     {
-        id: 'data',
-        text: '订单管理',
-        isMutilLevel: false,
-        children: [
-            {
-                href: "/admin/enquiries",
-                text: "Enquiries",
-                leaf: true
-            },
-            {
-                href: "/admin/sample_request",
-                text: "Sample Request",
-                leaf: true
-            },
-            {
-                href: "/admin/po_request",
-                text: "PO Request",
-                leaf: true
-            },
-            {
-                href: "/admin/rfq_requirement",
-                text: "RFQ Request",
-                leaf: true
-            }
-        ]
-    },
-    {
         id: 'admin',
         text: '支持与服务',
         isMutilLevel: false,
         children: [
-            {
-                href: "/admin/faqs",
-                text: "FAQ",
-                leaf: true
-            },
-            {
-                href: "/admin/subscriptions",
-                text: "Subscriptions",
-                leaf: true
-            },
-            {
-                href: "/admin/settings",
-                text: "全站设置",
-                leaf: true
-            },
             {
                 href: "/admin/accounts",
                 text: "管理员帐号",
