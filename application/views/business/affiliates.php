6702969232b4e6797201e51dccdfba39bc102345
neo <497172418@qq.com>
2015-12-09 16:20:07 +0800

    #招商加盟的banner图

--- application/views/business/affiliates.php
+++ application/views/business/affiliates.php
@@ -10,10 +10,10 @@
 			<!-- Wrapper for slides -->
 			<div class="carousel-inner" role="listbox">
 				<div class="item active">
-					<img src="/assets/img/product/ad-business.jpg">
+					<img src="/assets/img/product/business-1.jpg">
 				</div>
 				<div class="item">
-					<img src="/assets/img/product/ad-map.jpg">
+					<img src="/assets/img/product/business-2.jpg">
 				</div>
 			</div>
 
